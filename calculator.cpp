fb83fbbbd456c1ba28e13c01054b78fee8dd20c3
Aaditya-Yadav07 <113791335+Aaditya-Yadav07@users.noreply.github.com>
2023-11-08 18:39:53 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    double num1 , num2;
+    char oper;
+
+    cout << "Enter the first number:";
+    cin >> num1;
+
+    cout <<"Enter the second number:";
+    cin >> num2;
+
+    cout << "Enter the operation( + , - , * , /):";
+    cin >> oper;
+
+    switch (oper)
+    {
+    case '+':
+        cout << num1 << oper << num2 << "=" << num1+num2 << endl;
+        break;
+    case '-':
+        cout << num1 << oper << num2 << "=" << num1-num2 << endl;
+        break;
+    case '*':
+        cout << num1 << oper << num2 << "=" << num1*num2 << endl;
+        break;
+    case '/':
+        cout << num1 << oper << num2 << "=" << num1/num2 << endl;
+        break;
+    default:
+        break;
+    }
+
+}
